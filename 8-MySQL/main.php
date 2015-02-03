beaaf8a3c0424d76b1645649be812ba63f92fe48
Davide Fossacecchi <rocker.rabbit@gmail.com>
2015-02-03 23:30:57 +0100

    Use JSON for sending more data

--- 8-MySQL/main.php
+++ 8-MySQL/main.php
@@ -1,4 +1,6 @@
 <?php
-	if ($_POST["process"] == "login") echo "GRANTED";
-	if ($_POST["process"] == "save") echo "SAVED";
+	$message["data"]="some data";
+	if ($_POST["process"] == "login") $message["result"]="GRANTED";
+	if ($_POST["process"] == "save") $message["result"]="SAVED";
+	echo json_encode($message);
 ?>
